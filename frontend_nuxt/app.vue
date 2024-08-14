91fb32956dad7dcce924eeaecc2af382865964aa
mhdalik <alikaithayil2@gmail.com>
2024-08-14 20:07:54 +0530

    ➕Add: nuxtui

--- frontend_nuxt/app.vue
+++ frontend_nuxt/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UAlert
+      icon="i-heroicons-command-line"
+      color="primary"
+      variant="solid"
+      title="Heads up!"
+      description="You can add components to your app using the cli."
+    />
   </div>
 </template>
