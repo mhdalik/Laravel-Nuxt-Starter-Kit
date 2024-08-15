60948a74c4c638fd0ca4f9891d9a94151322ca9c
mhdalik <alikaithayil2@gmail.com>
2024-08-15 16:01:39 +0530

    Revert "Merge pull request #1 from mhdalik/laravel_nuxt_with_nuxtui"
    This reverts commit 7177a3c973e0f9e003a648956212cf037bacc0aa, reversing
    changes made to 1a5a3d147f6133e81ab6b0735818dd209bc21699.

--- frontend_nuxt/app.vue
+++ frontend_nuxt/app.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <UAlert
-      icon="i-heroicons-command-line"
-      color="primary"
-      variant="solid"
-      title="Heads up!"
-      description="You can add components to your app using the cli."
-    />
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
   </div>
 </template>
